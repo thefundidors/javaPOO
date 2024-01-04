6bb1cd127f7a397c8b57d907a06f92a1ea57d9b8
the fundidors <thefundidorsc@gmail.com>
2024-01-04 16:53:07 +0100

    Se instancia el objeto y se le asignan valores a los atributos

--- src/main/java/EjemploAutomovil.java
+++ src/main/java/EjemploAutomovil.java
@@ -1,5 +1,16 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
+        Automovil auto = new Automovil();
+        auto.fabricante="Renault";
+        auto.cilindrada=200;
+        auto.color="Rojo";
+        auto.modelo="Megane";
+
+        System.out.println("Los datos del automovil son:");
+        System.out.println("Fabricante: " + auto.fabricante);
+        System.out.println("Cilindrada: " + auto.cilindrada);
+        System.out.println("Color: " + auto.color);
+        System.out.println("Modelo: " + auto.modelo);
     }
 }
